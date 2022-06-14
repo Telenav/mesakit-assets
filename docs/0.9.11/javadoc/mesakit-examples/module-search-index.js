ebed557d406363984f2b3eca76f453503e4d1c0a
jonathanl-telenav <jonathanl@telenav.com>
2022-06-13 18:14:24 -0600

    📝 Remove older documentation builds

--- docs/0.9.11/javadoc/mesakit-examples/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"mesakit.map.examples"}];updateSearchResults();
--- docs/0.9.11/javadoc/mesakit-examples/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"mesakit.map.examples","l":"com.telenav.mesakit.examples.map.measurements"}];updateSearchResults();
